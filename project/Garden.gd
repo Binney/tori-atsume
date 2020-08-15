ebb229aebfb1bdef1cfcea0427a79b0de93a21d8
Sarah Binney <sarah.binney@softwire.com>
2020-08-15 11:53:41 +0100

    Generic burb and instanced Robin

--- project/Garden.gd
+++ project/Garden.gd
@@ -4,8 +4,9 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export (PackedScene) var Burd
+export (PackedScene) var Robin
 
+const ROBIN = preload("res://Robin.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():    
@@ -17,16 +18,28 @@ func new_game():
 
 	
 func _on_BurdTimer_timeout():
+	spawnRobin()
+	
+#	var burd = Burd.instance()
+#	add_child(burd)
+#	# Set the mob's direction perpendicular to the path direction.
+#	var direction = $BurdPath/SpawnBurdLocation.rotation + PI / 2
+#	# Set the mob's position to a random location.
+#	burd.position = $BurdPath/SpawnBurdLocation.position
+#	# Add some randomness to the direction.
+#	direction += rand_range(-PI / 4, PI / 4)
+#	burd.rotation = direction
+#	# Set the velocity (speed & direction).
+#	burd.linear_velocity = Vector2(rand_range(burd.min_speed, burd.max_speed), 0)
+#	burd.linear_velocity = burd.linear_velocity.rotated(direction)
+
+func spawnRobin():
 	$BurdPath/SpawnBurdLocation.offset = randi()
-	var burd = Burd.instance()
+	var burd = ROBIN.instance()
 	add_child(burd)
-	# Set the mob's direction perpendicular to the path direction.
 	var direction = $BurdPath/SpawnBurdLocation.rotation + PI / 2
 	# Set the mob's position to a random location.
 	burd.position = $BurdPath/SpawnBurdLocation.position
 	# Add some randomness to the direction.
 	direction += rand_range(-PI / 4, PI / 4)
 	burd.rotation = direction
-	# Set the velocity (speed & direction).
-	burd.linear_velocity = Vector2(rand_range(burd.min_speed, burd.max_speed), 0)
-	burd.linear_velocity = burd.linear_velocity.rotated(direction)
