ca08fc7e520304fa32ad6e632d2ed1443f68d4a3
Airlangga Gunawan <airlambda@gmail.com>
2020-08-16 16:38:23 +0100

    Fixed default SeedBucket problem

--- project/ShopScreen.gd
+++ project/ShopScreen.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # Declare member variables here. Examples:
-var current_item = "SeedBucket"
+var current_item = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,10 +9,12 @@ func _ready():
 
 func _on_Buy_button_up():
 	#BUY ITEMS
-	#InventoryManagement._add_to_inventory("SeedBucket")
-	#InventoryManagement._add_to_inventory("GenericItem")
-	print("Buying {str}".format({"str":current_item}))
-	InventoryManagement._add_to_inventory(current_item)
+	if len(current_item) == 0:
+		# Buyer has not chosen an item to buy
+		$NullBuyCheck.popup_centered()
+	else:
+		print("Buying {str}".format({"str":current_item}))
+		InventoryManagement._add_to_inventory(current_item)
 
 func _on_Item1_button_up():
 	print("Selecting SeedBucket")
@@ -34,17 +36,13 @@ func _on_Item5_button_up():
 	print("Selecting NestBox")
 	current_item = "NestBox"
 
-
 func _on_Item6_pressed():
 	print("Selecting WaterBucket")
 	current_item = "WaterBucket"
 
-
 func _on_Item7_pressed():
 	print("Selecting IceBucket")
 	current_item = "IceBucket"
-
-
 
 func _on_Item8_pressed():
 	print("Selecting Tree")
